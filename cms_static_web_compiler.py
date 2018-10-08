c7116a97a7d1bf20826a97ec97255557ec57b7d4
carlcastello <carl.castello@yahoo.com>
2018-10-08 01:31:17 +0000

    SED-1: Recursive directory creation

--- cms_static_web_compiler.py
+++ cms_static_web_compiler.py
@@ -1,12 +1,33 @@
+import os
 import sys
-from parser import read_file
 
-def create_project_structure():
+from dotenv import load_dotenv
+
+from parser import Parser
+from parser.local_parser import LocalParser
+
+load_dotenv(verbose=True)
+
+def get_parser(project_name: str, **kwargs: dict) -> Parser:
+    return {
+        'LOCAL_PARSER': LocalParser(project_name, **kwargs)
+    }[os.getenv('PARSER_ENV')]
+
+def create_project_structure(project_name: str, **kwargs: dict) -> None:
+    parser = get_parser(project_name, **kwargs)
+    parser.create_project_directories()
+
+def update_project(project_name: str) -> None:
     pass
 
-def update_project():
-    pass
 
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    terminal_parser = ArgumentParser()
+    
+    terminal_parser.add_argument('--name', help='The project file name', type=str)
+    terminal_parser.add_argument('--output_path', help='The project file name', type=str)
+    
+    terminal_arguments = terminal_parser.parse_args()
 
-if __name__ == "__main__":
-    pass
+    create_project_structure(terminal_arguments.name)
