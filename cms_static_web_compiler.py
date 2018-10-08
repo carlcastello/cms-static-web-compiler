bab08a58ef8b8411264dd0c9c22df32012224aa8
carlcastello <carl.castello@yahoo.com>
2018-10-08 00:30:26 +0000

    SED-1: Created an abstract class to be implemented with local and aws parsers and also deleted some useless files

---
+++ cms_static_web_compiler.py
@@ -0,0 +1,12 @@
+import sys
+from parser import read_file
+
+def create_project_structure():
+    pass
+
+def update_project():
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ parser/__init__.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Parser:
+
+    def __init__(self, project_name: str) -> None:
+        self._project_name = project_name
+
+    def generate_project_keys(self) -> List:
+        key_structure = "{}/{}/{}"
+        environments = ['develop', 'production']
+        folders = ['css', 'images', 'js']
+        
+        folder_keys = []
+        for environment in environments:
+            for folder in folders:
+                folder_keys.append(key_structure.format(self._project_name, environment, folder))
+
+        return folder_keys
+
+
+    def create_project_directories(self) -> None:
+        raise NotImplementedError
