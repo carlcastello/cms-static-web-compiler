7dc1a05be49c7309078259dff4da8a2de5a9532f
carlcastello <carl.castello@yahoo.com>
2018-10-08 05:59:22 +0000

    SED-1: Completed test of the compiler packeges

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,13 @@ from app.compiler.local_compiler import LocalCompiler
 
 load_dotenv(verbose=True)
 
-def get_parser(project_name: str, **kwargs: dict) -> Compiler:
+def _get_compiler(project_name: str, **kwargs: dict) -> Compiler:
     return {
-        'LOCAL_PARSER': LocalCompiler(project_name, **kwargs)
-    }[os.getenv('PARSER_ENV')]
+        'LOCAL_COMPILER': LocalCompiler(project_name, **kwargs)
+    }.get(os.getenv('COMPILER_ENV'), Compiler(project_name))
 
 def create_project_structure(project_name: str, **kwargs: dict) -> None:
-    parser = get_parser(project_name, **kwargs)
+    parser = _get_compiler(project_name, **kwargs)
     parser.create_project_directories()
 
 def update_project(project_name: str) -> None:
--- app/compiler/__init__.py
+++ app/compiler/__init__.py
@@ -5,7 +5,7 @@ class Compiler:
     def __init__(self, project_name: str) -> None:
         self._project_name = project_name
 
-    def generate_project_keys(self) -> List:
+    def _generate_project_keys(self) -> List[str]:
         key_structure = "{}/{}/{}"
         environments = ['develop', 'production']
         folders = ['css', 'images', 'js', 'robots']
@@ -19,4 +19,4 @@ class Compiler:
 
 
     def create_project_directories(self) -> None:
-        raise NotImplementedError
+        raise NotImplementedError('Trying to run compiler without a given environment')
