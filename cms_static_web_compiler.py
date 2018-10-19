37025f3eb055d1bc240419397d34481fca1cc938
carlcastello <carl.castello@yahoo.com>
2018-10-19 01:59:49 +0000

    SED-1: Docstrings in tha main py file'

--- cms_static_web_compiler.py
+++ cms_static_web_compiler.py
@@ -16,6 +16,7 @@ def cms_static_web_compiler() -> None:
         ArgParse type
         """
         # pylint: disable=too-few-public-methods
+
         name: str = ''
         output_path: str = ''
 
